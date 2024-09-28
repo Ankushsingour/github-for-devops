ab7fd75136436b798fb372e31b5b98b22876ab9e
ankush <singourankush123@gmail.com>
2024-09-28 16:47:17 +0530

    df

--- testing.py
+++ testing.py
@@ -1,6 +1,11 @@
+"""
+This module contains a simple demonstration of a hello function.
+"""
+
 def hello():
-     """
-     this is demo
-     """
-     return "hello"
+    """
+    This function returns a greeting message.
+    """
+    return "hello"
+
 hello()
